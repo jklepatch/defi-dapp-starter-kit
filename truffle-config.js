2c7b14c4f450ffbbb48587bbf0a58719d185965f
Julien Klepatch <julien@julienklepatch.com>
2019-11-19 20:14:13 +0800

    add ci

--- truffle-config.js
+++ truffle-config.js
@@ -4,14 +4,13 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
-  networks: {
-    develop: {
-      port: 8545
-    }
-  },
   compilers: {
     solc: {
       version: '0.5.12'
     }
-  }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions : { showTimeSpent:true }
+  },
 };
